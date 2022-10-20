f4dc0727cf252628b55aabcc91f8c1dad4b9f326
ssingh954 <112465470+ssingh954@users.noreply.github.com>
2022-10-20 11:33:58 +0530

    Add files via upload

--- q2.js
+++ q2.js
@@ -1,1 +1,15 @@
-//Q2. 
+//Q2. Create a function that finds the word "happiness" in the given string (not case sensitive). If found, return "Hurray!", otherwise return "There is no happiness.".
+
+function findHappiness(str){
+    const string1 = "happiness";
+    let index = str.indexOf(string1);
+    console.log(index);
+}
+var result = findHappiness("Work makes me happy");
+result = findHappiness("You give me the feeling of happiness");
+
+if(result == -1) {
+    console.log('There is no happiness.');
+} else {
+    console.log('Hurray!');
+}
